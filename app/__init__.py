099d671f884b02d6b5b0588ce34307990a93aeb9
hank <hancockgu1@gmail.com>
2024-11-25 22:50:49 +0800

    add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask#从flask包中导入Flask类
 from config import Config
+from flask_sqlalchemy import SQLAlchemy#从包中导入类
+from flask_migrate import Migrate
+import pymysql
+pymysql.install_as_MySQLdb()
+
 print(Config.SECRET_KEY)
 
 app = Flask(__name__)#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app.config.from_object(Config)
 print('等会谁（哪个包或模块）在使用我：',__name__)
 
+db = SQLAlchemy(app)#数据库对象
+migrate = Migrate(app, db)#迁移引擎对象
+
 #从app包中导入模块routes
-from app import routes
+from app import routes,models
 #注：上面两个app是完全不同的东西。两者都是纯粹约定俗成的命名，可重命名其他内容。
 
--- microblog.py
+++ microblog.py
@@ -1,5 +1,10 @@
 from app import app
 from app.forms import LoginForm
+from app.models import  User, Post, db
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db,'User':User,'Post':Post}
 
 if __name__ == '__main__':
     app.run(debug=True)
