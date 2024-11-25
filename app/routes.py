13397dc5038f5dcb670cf4849eb583f3b2de4ea5
hank <hancockgu1@gmail.com>
2024-11-25 22:10:01 +0800

    add form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app#从app包中导入 app这个实例
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import LoginForm
 
 #2个路由
 @app.route('/')
@@ -19,3 +20,10 @@ def index():
 	]
 	return render_template('index.html',title='Home',user=user,posts=posts)
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form = LoginForm() # 表单实例化对象
+	if form.validate_on_submit():
+		# flash('Login required for user {}, remember_me{}'.format(form.username.data,form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html',title='Sign In', form=form)
--- microblog.py
+++ microblog.py
@@ -1,4 +1,5 @@
 from app import app
+from app.forms import LoginForm
 
 if __name__ == '__main__':
     app.run(debug=True)
